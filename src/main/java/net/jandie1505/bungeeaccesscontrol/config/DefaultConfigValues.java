44e76f0d486d8ab17f7da2ddb9a0d4db4b34b986
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-02-09 22:04:19 +0100

    default config values

--- src/main/java/net/jandie1505/bungeeaccesscontrol/config/DefaultConfigValues.java
+++ src/main/java/net/jandie1505/bungeeaccesscontrol/config/DefaultConfigValues.java
@@ -9,7 +9,19 @@ public final class DefaultConfigValues {
     public static JSONObject getConfig() {
         JSONObject config = new JSONObject();
 
+        JSONObject mysqlConfig = new JSONObject();
+        mysqlConfig.put("host", "");
+        mysqlConfig.put("port", 3306);
+        mysqlConfig.put("username", "");
+        mysqlConfig.put("password", "");
+        mysqlConfig.put("database", "bungeeaccesscontrol");
+        config.put("mysql", mysqlConfig);
 
+        JSONObject disconnectScreensConfig = new JSONObject();
+        disconnectScreensConfig.put("lockdown", "This network is currently under lockdown");
+        disconnectScreensConfig.put("bannedPermanently", "You are permanently banned!\nReason: {reason}");
+        disconnectScreensConfig.put("bannedTemporary", "You are temporarily banned!\nReason: {reason}\nUntil: {until}");
+        config.put("disconnectScreens", disconnectScreensConfig);
 
         return config;
     }
