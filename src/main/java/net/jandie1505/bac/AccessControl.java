12386664004da448f4663c22421b37eadd6f0c58
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-02-09 21:12:30 +0100

    initial commit

---
+++ src/main/java/net/jandie1505/bac/AccessControl.java
@@ -0,0 +1,30 @@
+package net.jandie1505.bac;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class AccessControl extends Plugin {
+    private static AccessControl accessControl;
+
+    // PLUGIN
+
+    private boolean lockdown;
+
+    @Override
+    public void onEnable() {
+        this.lockdown = false;
+    }
+
+    public boolean isLockdown() {
+        return this.lockdown;
+    }
+
+    public void setLockdown(boolean lockdown) {
+        this.lockdown = lockdown;
+    }
+
+    // STATIC
+
+    public static AccessControl getInstance() {
+        return accessControl;
+    }
+}
